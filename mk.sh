1f9f3f95185e0cef5f6ce77acc67a3552d9f2c3a
Aaron Zhang <zhangrbing@gmail.com>
2014-01-15 22:35:20 +0800

    第五章例子程序中新增两个，关于设置sockopt选项的使用

---
+++ mk.sh
@@ -0,0 +1,9 @@
+# !/bin/sh
+
+#simple make command
+g++ 5-3-testlisten.c -o testlisten
+g++ 5-5-testaccept.c -o testaccept
+g++ 5-6-testoobsend.c -o testoobsend
+g++ 5-7-testoobrecv.c -o testoobrecv
+g++ 5-10-setsendbuffer.c -o setsendbuffer
+g++ 5-11-setrecvbuffer.c -o setrecvbuffer
