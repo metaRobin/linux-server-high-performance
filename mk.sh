506ba86896ff705955ace4dea999139b7327cd8c
Aaron Zhang <zhangrbing@gmail.com>
2014-01-16 22:17:04 +0800

    第六章第一个例子，关于dup使用

--- mk.sh
+++ mk.sh
@@ -10,3 +10,5 @@ g++ 5-6-testoobsend.c -o testoobsend
 g++ 5-7-testoobrecv.c -o testoobrecv
 g++ 5-10-setsendbuffer.c -o setsendbuffer
 g++ 5-11-setrecvbuffer.c -o setrecvbuffer
+g++ 5-12-daytime.c -o daytime
+g++ 6-1-dup-cgi.c -o  dup-cgi
